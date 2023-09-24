35034e17e3c627c4bd524f696d919a61953907a2
Luis Lescano <luislescanologo@hotmail.com>
2023-09-24 12:10:55 -0300

    add login for user api view

--- users/views.py
+++ users/views.py
@@ -11,7 +11,11 @@ from django.shortcuts import get_object_or_404
 class UserLoginView(APIView):
     def post(self, request):
         user = get_object_or_404(User, username=request.data['username'])
-        return Response({})
+        if not user.check_password(request.data['password']):
+            return Response({"detail": "User not found"}, status=HTTP_401_UNAUTHORIZED)
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(user)
+        return Response({"token": token.key, "user": serializer.data})
 
     
 class UserSignUpView(APIView):
@@ -27,3 +31,6 @@ class UserSignUpView(APIView):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
+class UserTokenTestView(APIView):
+    def post(request):
+        return Response({})
